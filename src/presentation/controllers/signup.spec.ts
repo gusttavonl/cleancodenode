d1c33da10a09de0205558fc9170e21084c8b88db
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-05-08 17:16:56 -0300

    test: signup controller return error if email not exists

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,14 +5,29 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'an_name',
-        email: 'an_email@email.com',
+        email: 'any_email@email.com',
         password: 'an_password',
         passowrdConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: nome'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'an_password',
+        passowrdConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
