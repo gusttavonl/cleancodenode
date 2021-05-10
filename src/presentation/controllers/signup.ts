070a4fb1914372beac5952af8146fe0fc8e73cc6
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-05-10 08:20:05 -0300

    feat: ensure signup controller is not exists confirmation password

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { badRequest } from '../helper/http-helper'
 import { MissingParamError } from '../error/missing-param-error'
 export class SignUpController{
   handle(httpRequest: HttpRequest): any{
-    const requiredFields = ['name','email', 'password']
+    const requiredFields = ['name','email', 'password', 'passowrdConfirmation']
     for (const field of requiredFields){
       if (!httpRequest.body[field]){
         return badRequest( new MissingParamError(field) )
