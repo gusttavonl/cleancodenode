29a76355ab672c9b7bd21618590b50ca2e9bf268
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-06-14 17:25:11 -0300

    refactor: move sut and encrypter to function make sut

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,27 @@
+import { Encrypter } from "../../protocols/encrypter"
 import { DbAddAccount } from "./db-add-account"
 
-describe('DbAddAccount Usecase', () => {
-  test('Expected Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string>  {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
+interface SutTypes {
+  sut: DbAddAccount,
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): any => {
+  class EncrypterStub {
+    async encrypt (value: string): Promise<string>  {
+      return new Promise(resolve => resolve('hashed_password'))
     }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut, encrypterStub 
+  }
+}
+describe('DbAddAccount Usecase', () => {
+  const {sut, encrypterStub } = makeSut()
+  test('Expected Encrypter with correct password', async () => {
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
