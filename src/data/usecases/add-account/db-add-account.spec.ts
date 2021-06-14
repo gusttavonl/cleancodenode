65fb72d5427b8979e50bc49bbf662671a37c9663
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-06-14 17:29:36 -0300

    refactor: move encrypter to function make encrypter

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -6,13 +6,19 @@ interface SutTypes {
   encrypterStub: Encrypter
 }
 
-const makeSut = (): any => {
-  class EncrypterStub {
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
     async encrypt (value: string): Promise<string>  {
       return new Promise(resolve => resolve('hashed_password'))
     }
   }
-  const encrypterStub = new EncrypterStub()
+
+  return new EncrypterStub()
+   
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
   const sut = new DbAddAccount(encrypterStub)
 
   return {
